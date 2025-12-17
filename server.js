5c1491a04abc0d5edd63623867cdf0e20cca5317
poobalana2024eee-coder <poobalan.a2024eee@sece.ac.in>
2025-12-17 15:17:09 +0530

    Delete service folder

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
 const express = require("express");
-const ProductRoute = require("./routes/productRoute")
-const UserRoute = require("./routes/userRoute")
+const ProductRoute = require("./routes/productRoute");
+const UserRoute = require("./routes/userRoute");
 const dotenv = require("dotenv");
 const connectdb = require("./config/db");
 const cors = require("cors");
 
 dotenv.config();
 connectdb();
+
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
-  credentials: true
+  origin: ['http://localhost:3000'],
+  credentials: true,
 }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", ProductRoute);
 app.use("/api", UserRoute);
-const PORT = process.env.PORT || 3000;
+
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Backend server running on port ${PORT}`);
 });
